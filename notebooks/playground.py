869764f29420fddee4c970a04741d4ec112350fb
Nick Giangregorio <ngiangregorio1@babson.edu>
2026-09-23 10:07:30 -0400

    created play ground notebook in class to mess around with

---
+++ notebooks/playground.py
@@ -0,0 +1,27 @@
+import marimo
+
+__generated_with = "0.24.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo 
+
+    return
+
+
+@app.cell
+def _():
+    charge = 10
+    return (charge,)
+
+
+@app.cell
+def _(charge):
+    print(charge)
+    return
+
+
+if __name__ == "__main__":
+    app.run()
